5b349f70f56fd28565ab093f8bb98b151599e71a
Jerry Pan <jerrykpan@gmail.com>
2021-06-30 19:20:14 -0400

    Added a help message to the SteamID command

--- app/steam_cog.py
+++ app/steam_cog.py
@@ -7,6 +7,6 @@ class SteamGames(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="getSteamID")
+    @commands.command(name="getSteamID", help="Returns your Steam # ID given your username")
     async def getSteamID(self, ctx, username):
         await ctx.send(steam.getSteamUserID(username))
